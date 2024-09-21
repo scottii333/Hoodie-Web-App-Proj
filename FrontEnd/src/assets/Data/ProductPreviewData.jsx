6a397aacc5aa0ccfcf6fbed0f79b6b30c10897cc
Scottiieee <splendidhoodies@gmail.com>
2024-09-21 12:29:18 +0800

    change the aboutPage design

---
+++ FrontEnd/src/assets/Data/ProductPreviewData.jsx
@@ -0,0 +1,30 @@
+import hoodieProdImg1 from "../Images/img1.png";
+import hoodieProdImg2 from "../Images/img2.png";
+import hoodieProdImg3 from "../Images/img3.png";
+
+export const AboutProductPreview = [
+  {
+    id: 1,
+    prodImage: hoodieProdImg1,
+  },
+  {
+    id: 2,
+    prodImage: hoodieProdImg2,
+  },
+  {
+    id: 3,
+    prodImage: hoodieProdImg3,
+  },
+  {
+    id: 4,
+    prodImage: hoodieProdImg1,
+  },
+  {
+    id: 5,
+    prodImage: hoodieProdImg2,
+  },
+  {
+    id: 6,
+    prodImage: hoodieProdImg3,
+  },
+];
--- FrontEnd/src/assets/Pages/AboutPage.jsx
+++ FrontEnd/src/assets/Pages/AboutPage.jsx
@@ -1,44 +1,18 @@
 import "../styles/About.css";
-import AboutImg1 from "../Images/img1.png";
-import AboutImg2 from "../Images/img2.png";
-import AboutImg3 from "../Images/img3.png";
-import AboutInsta from "../Images/insta.png";
-import AboutFb from "../Images/fb.png";
-import AboutTiktok from "../Images/tiktok.png";
+import { AboutProductPreview } from "../Data/ProductPreviewData";
+
 export const AboutSect = () => {
   return (
-    <section className="About-Section">
-      <div className="About-Images">
-        <img
-          src={AboutImg1}
-          style={{ width: "100%", height: "100%", borderRadius: "1.5rem" }}
-        />
+    <div className="Product-Parent-Container">
+      <div className="Product-Preview-Container">
+        {AboutProductPreview.map((product) => (
+          <img
+            key={product.id}
+            src={product.prodImage}
+            className="Product-About-Image"
+          />
+        ))}
       </div>
-      <div className="About-Greeting">
-        <h2>Lorem ipsum dolor si amet</h2>
-        <p>
-          A hoodie business specializes in designing, producing, and selling
-          various styles of hoodies, It often focuses on creating unique
-        </p>
-        <button>Discover more!</button>
-        <div className="About-SocialMedia">
-          <img src={AboutInsta} style={{ height: "1.5rem" }} />
-          <img src={AboutFb} style={{ height: "1.5rem" }} />
-          <img src={AboutTiktok} style={{ height: "1.5rem" }} />
-        </div>
-      </div>
-      <div className="About-Images">
-        <img
-          src={AboutImg2}
-          style={{ width: "100%", height: "100%", borderRadius: "1.5rem" }}
-        />
-      </div>
-      <div className="About-Images">
-        <img
-          src={AboutImg3}
-          style={{ width: "100%", height: "100%", borderRadius: "1.5rem" }}
-        />
-      </div>
-    </section>
+    </div>
   );
 };
